b32ebccba9c67c5f0d591ec80d2a9ff1864a45f8
nklimko <nklimko@clemson.edu>
2022-11-01 12:06:35 -0400

    11/1 workflowr not implemented, discuss

--- qtl-starve.R
+++ qtl-starve.R
@@ -1,41 +1,66 @@
-#use this to save plots to secretariat somehow
+rm(list=ls())
+
+#libraries
 library(ggplot2)
 library(dplyr)
 library(tidyr)
+#library("workflowr")
+
+
+#install.packages("workflowr")
+
 rm(list=ls())
 
-par(mfrow=c(2,2))
+par(mfrow=c(2,3))
 
-histBox <- function(csvPath, dataSet){
+#create csv from hist
+histCsv <- function(csvPath, dataSet){
   
   #Read in table
   allRaw <- tibble(read.csv(csvPath))
   
   starveRaw <- allRaw %>% select(line,starvation) %>% filter(!is.na(starvation))
   
-  #histogram and boxplot
+  #histogram
   hist(starveRaw$starvation, main=paste("Histogram of",dataSet,"Starvation"), xlab="Starvation Resistance")
-  #boxplot(starveRaw$starvation, , horizontal = TRUE)
   return(starveRaw)
 }
 
+#save tibble
+saveTib <- function(fileName, tib){
+  fullName <- paste("/home/nklimko/R/dgrp-starve/", fileName, ".csv", sep = "")
+  write.csv(tib,fullName)
+  return()
+}
+
+#restore tibble
+restore <- function(fileName){
+  fullName <- paste("/home/nklimko/R/dgrp-starve/", fileName, ".csv", sep = "")
+  tib <- tibble(read.csv(fullName))
+  return(tib)
+}
 
 #Male case
 csvPathM <- "/data/morgante_lab/data/dgrp/phenotypes/eQTL_traits_males.csv"
 dataSetM <- "Male"
+starveRawM <- histCsv(csvPathM, dataSetM)
 
 #Female case
 csvPathF <- "/data/morgante_lab/data/dgrp/phenotypes/eQTL_traits_females.csv"
 dataSetF <- "Female"
+starveRawF <- histCsv(csvPathF, dataSetF)
 
+#Both case
+starveBoth <- tibble(rbind(starveRawM, starveRawF))
+hist(starveBoth$starvation, main=paste("Histogram of Starvation"), xlab="Starvation Resistance")
 
-starveRawM <- histBox(csvPathM, dataSetM)
-starveRawF <- histBox(csvPathF, dataSetF)
+#Comparative Boxplot
+boxplot(starveRawM$starvation, starveRawF$starvation, starveBoth$starvation, names=c("Male", "Female", "Both"), main="Sex Comparison of Starvation", xlab="Starvation Resistance", ylab="Sex", horizontal = TRUE)
 
-boxplot(starveRawM$starvation, starveRawF$starvation, names=c("Male", "Female"), main="Sex Comparison of Starvation Resistance", xlab="Starvation Resistance", ylab="Sex", horizontal = TRUE)
-
+#Summary stats
 summary(starveRawM$starvation)
 summary(starveRawF$starvation)
+summary(starveBoth$starvation)
 
 colnames(starveRawF) <- c("line", "fStarve")
 colnames(starveRawM) <- c("line", "mStarve")
@@ -43,12 +68,14 @@ colnames(starveRawM) <- c("line", "mStarve")
 
 starveAll <- starveRawF %>% mutate(mStarve = starveRawM$mStarve)
 
-starveAll
+starveDiff <- starveAll %>% select(line, fStarve, mStarve) %>% mutate(diffStarve = fStarve- mStarve) 
 
-write.csv(starveAll,"/home/nklimko/R/dgrp-starve/starveAll.csv")
-write.csv(starveRawM,"/home/nklimko/R/dgrp-starve/starveM.csv")
-write.csv(starveRawF,"/home/nklimko/R/dgrp-starve/starveF.csv")
+hist(starveDiff$diffStarve, main=paste("Histogram of Starvation Diff"), xlab="Starvation Difference")
 
-mFin <- tibble(read.csv("/home/nklimko/R/dgrp-starve/starveM.csv"))
-fFin <- tibble(read.csv("/home/nklimko/R/dgrp-starve/starveF.csv"))
-aFin <- tibble(read.csv("/home/nklimko/R/dgrp-starve/starveAll.csv"))
+
+saveTib("starveDiff", starveDiff)
+dFin <- restore("starveDiff")
+starveDiff <- dFin %>% select(line, fStarve, mStarve, diffStarve)
+
+#starveDiff
+
